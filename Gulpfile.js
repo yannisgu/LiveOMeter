65ce1ca712f67dd9f371c885382236eec97a6d6a
Yannis Guedel <me@yannisguedel.ch>
2015-10-06 01:15:37 +0200

    changed pack system to browserify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,29 @@
+'use strict';
+
+var browserify = require('browserify');
 var gulp = require('gulp');
-var babel = require('gulp-babel');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var babelify = require('babelify');
 
 gulp.task('default', function () {
-    return gulp.src('src/**/*.js')
-        .pipe(babel({modules: 'amd'}))
-        .pipe(gulp.dest('dist'));
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: './src/browser.js',
+    debug: true,
+    // defining transforms here will avoid crashing your stream
+    transform: [babelify]
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+//    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
 });
